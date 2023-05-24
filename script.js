ac088d312d8e2140adb8f5e9851d5413512a8d2c
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-05-24 16:55:06 -0400

    Added an array of objects containing the quiz questions

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ startBtn.on("click", function () {
 });
 
 // Add array of objects for questions
+var quizQuestions = [
+  {
+    question:
+      "Which of the following is not a common data type used in JavaScript?",
+    choices: ["1. Boolean", "2. Number", "3. Double", "4. String"],
+    correct: "3. Double",
+  },
+  {
+    question: "Which language is used to add styles to webpages?",
+    choices: ["1. JavaScript", "2. CSS", "3. Python", "4. HTML"],
+    correct: "2. CSS",
+  },
+  {
+    question: "What symbol is used to set apart blocks of code?",
+    choices: ["1. ( )", "2. | |", "3. [ ]", "4. { }"],
+    correct: "4. { }",
+  },
+  {
+    question:
+      "What command is a helpful tool for debugging code as you go along?",
+    choices: [
+      "1. console.log",
+      "2. Math.floor",
+      "3. array.length",
+      "4. window.object",
+    ],
+    correct: "1. console.log",
+  },
+  {
+    question: "What types of data can be stored in an array?",
+    choices: [
+      "1. Booleans",
+      "2. Numbers",
+      "3. Strings",
+      "4. All of the above!",
+    ],
+    correct: "4. All of the above!",
+  },
+];
